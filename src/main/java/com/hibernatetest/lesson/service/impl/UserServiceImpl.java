936c1c3e1ea1fd96c122fa2e413c68bc687d5554
Roman Bogatyrev <manhunt10@yandex.ru>
2021-05-09 19:00:07 +0400

    [HP02-005] added logger

--- src/main/java/com/hibernatetest/lesson/service/impl/UserServiceImpl.java
+++ src/main/java/com/hibernatetest/lesson/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.hibernatetest.lesson.repository.UserRepository;
 import com.hibernatetest.lesson.service.UserService;
 import com.hibernatetest.lesson.web.entity.UserDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
@@ -18,6 +19,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -27,20 +29,27 @@ public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
 
     public Optional<User> saveUser(User user) {
+        log.debug("user is id {} common", user.getId());
         Optional<User> byId = userRepository.findById(user.getId());
         if (byId.isEmpty()) {
             User userResult = userRepository.saveAndFlush(user);
+            log.info("user has saved by id {}", userResult.getId());
             return Optional.of(userResult);
         }
+        log.info("user by id {} is found, not saved", user.getId());
         throw new MyCustomException("the user already exists");
     }
 
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        List<User> allUsers = userRepository.findAll();
+        log.info("size all users is: {}", allUsers.size());
+        return allUsers;
     }
 
     public Optional<User> getUserById(UUID uuid) {
-        return userRepository.findById(uuid);
+        Optional<User> user = userRepository.findById(uuid);
+        log.info("user by id {}, has found: {}", uuid, user);
+        return user;
     }
 
     public Optional<User> getUserFromCriteria(UUID uuid) {
@@ -54,14 +63,17 @@ public class UserServiceImpl implements UserService {
 
     public void deleteUser(UUID uuid) {
         userRepository.deleteById(uuid);
+        log.info("user by id: {}, has deleted", uuid);
     }
 
     public Optional<User> updatedUser(UserDto userDto) {
         Optional<User> user = userRepository.findById(userDto.getId());
         if (user.isPresent()) {
             userMapper.copyUser(user.get(), userDto);
+            log.info("user by id {}, has updated", user.get().getId());
             return Optional.of(userRepository.saveAndFlush(user.get()));
         }
+        log.info("user id: {} not found", userDto.getId());
         throw new MyCustomException("User id: " + userDto.getId() + " not found");
     }
 }
